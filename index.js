159d99b90ddbb744728711626b9d9a5b9a5ceb02
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-03 09:18:37 -0300

    leitor escrito de json

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const fs = require("fs");
 
-fs.writeFile("./texto.txt", "Novo conteúdo do arquivo!", (err) => {
-    if (err) {
-        console.log("Erro durante o salvamento...");
-    }
-});
+function modificarUsuario(nome, curso, categoria) {
+    fs.readFile("./usuario.json", {encoding: 'utf-8'}, (err, data) => {
+        if (err) {
+            console.log("Um erro aconteceu!");
+        } else {
+            var content = JSON.parse(data);
+    
+            content.nome = nome;
+            content.curso = curso;
+            content.categoria = categoria;
+    
+            console.log(content);
+    
+            fs.writeFile("./usuario.json", JSON.stringify(content), (err) => {
+                if (err) {
+                    console.log("Um erro aconteceu durante a escrita!");
+                }
+            });
+        }
+    })
+}
+
+modificarUsuario("Juliano", "Formação Node Js", "Curso da Udemy");
