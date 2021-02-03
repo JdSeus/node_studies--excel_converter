528186b2779f80836d58f74cc50f7e713a346a74
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-03 10:52:29 -0300

    table

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var Reader = require("./Reader.js");
 var Processor = require("./Processor");
+var Table = require("./Table");
 
 var leitor = new Reader();
 
 async function main() {
     var dados = await leitor.Read("./users.csv");
     var dadosProcessados = Processor.Process(dados);
+
+    var usuarios = new Table(dadosProcessados);
+    console.log(usuarios.header);
+    console.log(usuarios.rows);
 }
 
 main();
