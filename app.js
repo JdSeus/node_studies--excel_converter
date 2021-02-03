e377463cff14140df63d7fdfe28a99a9d124dd50
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-03 11:26:04 -0300

    writer

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ var Reader = require("./Reader.js");
 var Processor = require("./Processor");
 var Table = require("./Table");
 var HtmlParser = require("./HtmlParser");
+var Writer = require("./Writer");
 
 var leitor = new Reader();
+var escritor = new Writer();
 
 async function main() {
     var dados = await leitor.Read("./users.csv");
     var dadosProcessados = Processor.Process(dados);
+
     var usuarios = new Table(dadosProcessados);
+
     var html = await HtmlParser.Parse(usuarios);
 
-    console.log(html);
+    escritor.Write(Date.now() + ".html", html);
 }
 
 main();
